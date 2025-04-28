6186b99b5121bba4abb259a4588ae4e24bbcf848
Artemiy <rt.rk.ra@gmail.com>
2025-04-28 13:53:32 +0300

    initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+
+    protected $fillable = [
+        'name',
+        'birth_date',
+        'phone',
+        'email',
+        'login',
+        'password',
+        'image_path',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
